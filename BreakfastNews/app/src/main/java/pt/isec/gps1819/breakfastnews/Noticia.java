16f04e66dc8caf3070a7638ac332bf93994eed24
Diogo <diogoplima9@gmail.com>
2018-11-22 16:49:15 +0000

    selecao de noticias
    inicio madeiras

---
+++ BreakfastNews/app/src/main/java/pt/isec/gps1819/breakfastnews/Noticia.java
@@ -0,0 +1,10 @@
+package pt.isec.gps1819.breakfastnews;
+
+import android.media.Image;
+
+class Noticia {
+    String titulo;
+    String descricao;
+    String corpo;
+    Image imagens;
+}
---
+++ BreakfastNews/app/src/main/java/pt/isec/gps1819/breakfastnews/SelecionaNoticias.java
@@ -0,0 +1,13 @@
+package pt.isec.gps1819.breakfastnews;
+
+import java.util.List;
+
+public class SelecionaNoticias {
+    List<Noticia> noticias;
+
+    boolean obtemNoticia(){
+
+
+        return false;
+    }
+}
