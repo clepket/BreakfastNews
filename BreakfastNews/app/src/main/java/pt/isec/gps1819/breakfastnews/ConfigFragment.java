16ced8541e2e7bd0479069406c6c673a821ac2d4
Diogo <diogoplima9@gmail.com>
2018-12-10 22:11:04 +0000

    perfil corrigido
    correção

--- BreakfastNews/app/src/main/java/pt/isec/gps1819/breakfastnews/ConfigFragment.java
+++ BreakfastNews/app/src/main/java/pt/isec/gps1819/breakfastnews/ConfigFragment.java
@@ -63,25 +63,29 @@ public class ConfigFragment extends Fragment implements View.OnClickListener , T
 
         buffer = ((MainActivity)getActivity()).readFile("perfil.txt");
 
-        linhas = buffer.split("\n");
-
         //variáveis para guardar perfil no ficheiro
         et_keywords = (EditText) v.findViewById(R.id.et_keywords);
         et_journalists = (EditText) v.findViewById(R.id.et_journalists);
         btn_config_guardar = (Button) v.findViewById(R.id.btn_config_guardar);
         image_btn_clock = (ImageButton) v.findViewById(R.id.image_btn_clock);
 
-        if(linhas.length > 1) {
+        if(buffer != null) {
+            linhas = buffer.split("\n");
+
             et_keywords.setText(linhas[0]);
-            et_journalists.setText(linhas[1]);
-        }
 
-        if(linhas.length > 2) {
-            CheckBox checkBox = (CheckBox) v.findViewById(R.id.checkBox);
+            if(linhas.length > 1) {
+                et_journalists.setText(linhas[1]);
+            }
 
-            checkBox.setText("Todos os dias às " + linhas[2] + "h" + linhas[3]);
-            checkBox.setChecked(true);
+            if(linhas.length > 2) {
+                CheckBox checkBox = (CheckBox) v.findViewById(R.id.checkBox);
+
+                checkBox.setText("Todos os dias às " + linhas[2] + "h" + linhas[3]);
+                checkBox.setChecked(true);
+            }
         }
+        
 
         //button listener
         btn_config_guardar.setOnClickListener(this);
