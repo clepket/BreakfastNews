49f0fce23a3bd4147e0a95cb619ceacdda49b124
LeoOutlaw <leorr43@gmail.com>
2018-12-11 19:07:21 +0000

    Notificaçoes

--- BreakfastNews/app/src/main/java/pt/isec/gps1819/breakfastnews/MainActivity.java
+++ BreakfastNews/app/src/main/java/pt/isec/gps1819/breakfastnews/MainActivity.java
@@ -1,6 +1,10 @@
 package pt.isec.gps1819.breakfastnews;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.app.TimePickerDialog;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v4.app.FragmentManager;
@@ -23,11 +27,14 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener , TimePickerDialog.OnTimeSetListener {
 
+    int  hour ;
+    int min ;
     String hora = " ";
     String minuto = " ";
 
@@ -167,13 +174,37 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
-    @Override
     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        CheckBox checkBox = findViewById(R.id.checkBox);
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.HOUR_OF_DAY, hourOfDay);
+        c.set(Calendar.MINUTE, minute);
+        c.set(Calendar.SECOND, 0);
+        hour = hourOfDay;
+        min = minute;
         hora = Integer.toString(hourOfDay);
         minuto = Integer.toString(minute);
-        CheckBox checkBox = (CheckBox) findViewById(R.id.checkBox);
-
         checkBox.setText("Todos os dias às " + hourOfDay + "h" + minute);
         checkBox.setChecked(true);
+
+        if(checkBox.isChecked()) {
+            setAlarm(c);
+            Toast.makeText(this, "Alarme para as "+ hour+ "h"+min, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void setAlarm(Calendar c){
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(this, AlertReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 1, intent, 0);
+
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, c.getTimeInMillis(), pendingIntent);
+    }
+
+    public void alarmCancel() {
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(this, AlertReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 1, intent, 0);
+
     }
 }
